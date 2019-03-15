13f05a93de60e69688cae370c0092f0e350de218
joycevw02 <jwang01@stuy.edu>
2019-03-15 14:53:03 -0400

    Started helper

--- Merge.java
+++ Merge.java
@@ -1,4 +1,9 @@
 public class Merge{
+  private static int[] left;
+  private static int[] right;
+  private static int[] temp;
+  private static boolean leftsort = false;
+  private static boolean rightsort = false;
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
     merge(data, 0, data.length - 1);
@@ -11,6 +16,24 @@ public class Merge{
   }
 
   private static int help(int[] data, int low, int high){
-
+    if (low == high){
+      return low;
+    }
+    int middle = (data.length / 2) - 1;
+    if (data.length % 2 == 0){
+      left = new int[data.length / 2];
+      right = left;
+    }
+    else{
+      left = new int[middle];
+      right = new int[middle + 1];
+    }
+    temp = new int[data.length];
+    if (leftsort && rightsort){
+      for (int i = 0; i < temp.length; i ++){
+        
+      }
+    }
+    return middle;
   }
 }
