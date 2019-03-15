903a3e0933db668cbdd79673004791bfb87096b2
joycevw02 <jwang01@stuy.edu>
2019-03-15 14:40:06 -0400

    Empty mergehelper

--- Merge.java
+++ Merge.java
@@ -1,6 +1,10 @@
 public class Merge{
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
-    
+
+  }
+
+  private static void mergehelp(int[] data, int lo , int hi){
+
   }
 }
