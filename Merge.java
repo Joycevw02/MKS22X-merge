8744c212cf832706472fc477bf7b3f0410f86eb7
Joyce Wang <jwang01@stuy.edu>
2019-03-20 17:20:56 -0400

    Wrote new version, needs to fix an error

--- Merge.java
+++ Merge.java
@@ -1,23 +1,43 @@
 public class Merge{
-  private static int[] left;
-  private static int[] right;
-  private static int[] temp;
-  private static boolean leftsort = false;
-  private static boolean rightsort = false;
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
-    merge(data, 0, data.length - 1);
+    int[] temp = data;
+    mergesort(temp, data, 0, data.length - 1);
   }
 
   private static void mergesort(int[] data, int[] temp, int lo, int hi){
     if (lo >= hi){
       return;
     }
-    mergesort(temp, data, lo, ((lo + hi) / 2));
-    mergesort(temp, data, (((lo + hi) / 2) + 1), hi);
-    merge(data, lo, hi);
+    int middle = (lo + hi) / 2;
+    mergesort(temp, data, lo, middle);
+    mergesort(temp, data, middle + 1, hi);
+    merge(data, temp, lo, middle, hi);
   }
-  private static void merge(int[] data, int low , int high){
-    
+  private static void merge(int[] data, int[] temp, int lo , int mid, int hi){
+    int low = lo;
+    int middle = mid + 1;
+    for (int i = lo; i <= hi; i ++){
+      if (low <= mid && middle <= hi){
+        if (data[lo] <= data[middle]){
+          temp[i] = data[low];
+          low ++;
+        }
+        else {
+          temp[i] = data[middle];
+          middle ++;
+        }
+      }
+      else {
+        if (low > mid){
+          temp[i] = data[middle];
+          middle ++;
+        }
+        else{
+          temp[i] = data[low];
+          low ++;
+        }
+      }
+    }
   }
 }
